549ba843de931b5dd82d13a1c193e5d28fa96202
Adem <ademakyasar@gmail.com>
2024-10-14 17:44:35 -0400

    made discriminator

--- model.py
+++ model.py
@@ -1,3 +1,20 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+
+
+
+
+class Discriminator(nn.Module):
+
+    def __init__(self, in_features) -> None:
+        super().__init__()
+        self.disc = nn.Sequential(
+
+            nn.Linear(in_features,128),
+            nn.LeakyReLU(0.1),
+            nn.Linear(128,1),
+            nn.Sigmoid() #Ensures output is in [0,1]
+        )
+
+        def forward(self):
+
